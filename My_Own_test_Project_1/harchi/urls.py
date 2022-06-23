6ab7a7bfaab3b71163e5d2719091caf05af353eb
mohammadpmf <mohammad.pfallah@gmail.com>
2022-06-23 13:55:15 +0430

    ersale arguman e url ham dorost kardam tooye harchi

--- My_Own_test_Project_1/harchi/urls.py
+++ My_Own_test_Project_1/harchi/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from varzesh3 import views
+from . import views
 
 urlpatterns = [
-    path('', views.show_varzesh3, name='varzesh3'),
+    path('hello', views.say_hello, name='hello'),
+    path('goodbye', views.say_goodbye, name='goodbye'),
+    path('ok', views.say_ok, name='ok'),
+    path('im_good', views.say_im_good, name='im_good'),
+    path('im_good/<pk>', views.say_im_good_pk, name='im_good_pk'),
+    path('im_good/<int:pk>', views.say_im_good_int_pk, name='im_good_int_pk'),
+    path('im_good/<str:pk>', views.say_im_good_str_pk, name='im_good_str_pk'),
+    
+
+    
+    # in payiini ha ro zadam nadash. error mideh. alaki ham dg nazanam. hamoon int va addi kafiyeh.
+    # path('im_good/<bool:pk>', views.say_im_good_bool_pk, name='im_good_bool_pk'),
+    # path('im_good/<float:pk>', views.say_im_good_float_pk, name='im_good_float_pk'),
 ]
--- My_Own_test_Project_1/harchi/views.py
+++ My_Own_test_Project_1/harchi/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def say_hello(request):
+    return HttpResponse('Say Hello :D')
+
+def say_goodbye(request):
+    return HttpResponse('Say Goodbye :D')
+
+def say_ok(request):
+    return HttpResponse('Say OK :D')
+
+def say_im_good(request):
+    return HttpResponse('Say I\'m Good :D')
+
+def say_im_good_pk(request, pk):
+    return HttpResponse(f'Say I\'m Good :D {pk}')
+
+def say_im_good_int_pk(request, pk):
+    return HttpResponse(f'Say I\'m Good :D {pk}')
+
+def say_im_good_str_pk(request, pk):
+    return HttpResponse(f'Say I\'m Good :D {pk}')
+
+
+
+# kar nemikard in. dige baghia ro edameh nadam. hamoonja tooye urls.py behem error midad.
+# def say_im_good_float_pk(request, pk):
+#     return HttpResponse(f'Say I\'m Good :D {pk}')
