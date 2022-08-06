50233bd9a4bb35e04fcc7b504f5c82feccbbe12c
poulstar <poulstar@gmail.ocm>
2022-08-06 16:01:09 +0430

    bad az ezafe kardane post.

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 def post_list_view(request):
     # posts_list = Post.objects.all() # Hama ro migire.
@@ -26,3 +27,15 @@ def post_detail_view(request, pk):
     # Raveshe 3
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_create_view(request):
+    # print(request.method)
+    if request.method == 'POST':
+        print(request.POST)
+        post_title = request.POST.get('title')
+        post_text = request.POST.get('text')
+        Post.objects.create(title=post_title, text=post_text, author=User.objects.all()[0], status='pub')
+    elif request.method == 'GET':
+        print("GET request.")
+    return render(request, 'blog/post_create.html')
