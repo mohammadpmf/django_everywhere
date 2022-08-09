e365bc1671889f095beea495f238ae9a22eeaba9
sinabakhshandeh <sinabakhshandeht@gmail.com>
2022-08-09 14:22:52 +0430

    forms.py ro ham sakhtam va ezafe kardam.

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from .models import Post
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+
+from .models import Post
+from .forms import NewPostForm
 
 def post_list_view(request):
     # posts_list = Post.objects.all() # Hama ro migire.
@@ -29,13 +31,26 @@ def post_detail_view(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+# Raveshe 1
+# def post_create_view(request):
+#     # print(request.method)
+#     if request.method == 'POST':
+#         print(request.POST)
+#         post_title = request.POST.get('title')
+#         post_text = request.POST.get('text')
+#         Post.objects.create(title=post_title, text=post_text, author=User.objects.all()[0], status='pub')
+#     elif request.method == 'GET':
+#         print("GET request.")
+#     return render(request, 'blog/post_create.html')
+
+# Raveshe 2
 def post_create_view(request):
-    # print(request.method)
     if request.method == 'POST':
-        print(request.POST)
-        post_title = request.POST.get('title')
-        post_text = request.POST.get('text')
-        Post.objects.create(title=post_title, text=post_text, author=User.objects.all()[0], status='pub')
-    elif request.method == 'GET':
-        print("GET request.")
-    return render(request, 'blog/post_create.html')
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = NewPostForm()
+            
+    else:
+        form = NewPostForm()
+    return render (request, 'blog/post_create.html', context={'form': form})
