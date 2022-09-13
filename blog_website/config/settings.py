acc76844b7beee5c556e6df3af408f1095e9ee82
sinabakhshandeh <sinabakhshandeht@gmail.com>
2022-09-13 14:20:59 +0430

    22 shahrivar

--- blog_website/config/settings.py
+++ blog_website/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # My apps
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "posts_list"
+LOGOUT_REDIRECT_URL = "posts_list"
